07553fe1b0b87062a81fbeeeea50b92d978d50ad
Mluthfirizky <luthfi.ginting@icloud.com>
2021-12-10 14:39:34 +0700

    Initial commit

---
+++ app/src/main/java/learn/idn/resep_app/MainActivity.kt
@@ -0,0 +1,60 @@
+package learn.idn.resep_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvResep: RecyclerView
+    private var list: ArrayList<Resep> = arrayListOf()
+    private var tempList:ArrayList<Resep> =  arrayListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val actionbar = supportActionBar
+        actionbar!!.title= "Home"
+
+        rvResep = findViewById(R.id.rvResep)
+        rvResep.setHasFixedSize(true)
+        list.addAll(ResepData.listData)
+        tempList.addAll(list)
+        showRecyclerList()
+
+    }
+    private fun showRecyclerList() {
+        rvResep.layoutManager = LinearLayoutManager(this)
+        val listResepAdapter = ListResepAdapter(list)
+        rvResep.adapter = listResepAdapter
+
+        listResepAdapter.setOnItemClickCallback(object : ListResepAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Resep) {
+                showResepSelected(data)
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+
+    private fun showResepSelected(resep: Resep) {
+        Toast.makeText(this, "Kamu memilih " + resep.name, Toast.LENGTH_SHORT).show()
+        val moveData = Intent(this, DetailActivity::class.java)
+        moveData.putExtra(DetailActivity.EXTRA_NAME, resep.name)
+        moveData.putExtra(DetailActivity.EXTRA_PHOTO, resep.photo)
+        moveData.putExtra(DetailActivity.EXTRA_DETAIL, resep.detail)
+        startActivity(moveData)
+    }
+}
---
+++ app/src/main/java/learn/idn/resep_app/Resep.kt
@@ -0,0 +1,7 @@
+package learn.idn.resep_app
+
+data class Resep(
+    var name: String = "",
+    var detail: String = "",
+    var photo: Int = 0
+)
